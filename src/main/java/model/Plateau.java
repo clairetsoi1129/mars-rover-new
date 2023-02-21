ba6456742e0c0a5ddc29bdb2dbf659c60e930671
Claire Tsoi <clairetsoi1129@gmail.com>
2023-02-21 13:34:51 +0000

    Add - Validation for positive width and height

--- src/main/java/model/Plateau.java
+++ src/main/java/model/Plateau.java
@@ -1,16 +1,36 @@
 package model;
 
+import exception.ValidationException;
+import javax.validation.*;
+import javax.validation.constraints.Min;
 import java.awt.*;
+import java.util.Set;
 
 public class Plateau {
+    @Min(value = 1, message = "Width should be greater than 1")
+    private int width;
+
+    @Min(value = 1, message = "Height should be greater than 1")
+    private int height;
+
     private Dimension size;
-    public Plateau (Dimension size){
-        this.size = size;
+
+    public Plateau (int width, int height) throws ValidationException {
+        this.width = width;
+        this.height = height;
+        validate();
+        this.size = new Dimension(width, height);
     }
     public Dimension getSize() {
         return size;
     }
 
-
-
+    public void validate() throws ValidationException{
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<Plateau>> violations = validator.validate(this);
+        for (ConstraintViolation<Plateau> violation : violations) {
+            throw new ValidationException(violation.getMessage());
+        }
+    }
 }
--- src/test/java/TestPlateau.java
+++ src/test/java/TestPlateau.java
@@ -1,23 +1,77 @@
+import exception.ValidationException;
 import model.Plateau;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPlateau {
     @Test
     void testNormalSquarePlateauSize() {
-        Dimension dim = new Dimension(5,5);
-        Plateau plateau = new Plateau(dim);
-        assertEquals(dim, plateau.getSize());
+        try {
+            Plateau plateau = new Plateau(5, 5);
+            assertEquals(new Dimension(5, 5), plateau.getSize());
+        }catch (ValidationException ignored){
+
+        }
     }
 
     @Test
     void testNormalRectanglePlateauSize() {
-        Dimension dim = new Dimension(8,5);
-        Plateau plateau = new Plateau(dim);
-        assertEquals(dim, plateau.getSize());
+        try {
+            Plateau plateau = new Plateau(8,5);
+            assertEquals(new Dimension(8,5), plateau.getSize());
+        }catch (ValidationException ignored){
+
+        }
     }
 
+    @Test
+    void testNegativePlateauWidth() {
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            Plateau plateau = new Plateau(-1,1);
+        });
+
+        String expectedMessage = "Width should be greater than 1";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage,expectedMessage);
+    }
+
+    @Test
+    void testNegativePlateauHeight() {
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            Plateau plateau = new Plateau(1,-1);
+        });
+
+        String expectedMessage = "Height should be greater than 1";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage,expectedMessage);
+    }
+
+    @Test
+    void testZeroPlateauHeight() {
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            Plateau plateau = new Plateau(0,1);
+        });
+
+        String expectedMessage = "Width should be greater than 1";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage,expectedMessage);
+    }
+
+    @Test
+    void testZeroPlateauWidth() {
+        Exception exception = assertThrows(ValidationException.class, () -> {
+            Plateau plateau = new Plateau(1,0);
+        });
+
+        String expectedMessage = "Height should be greater than 1";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage,expectedMessage);
+    }
 }
